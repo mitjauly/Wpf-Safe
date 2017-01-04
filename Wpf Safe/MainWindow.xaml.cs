f452250fc6407d84ca7936eb40e8f76e71489746
D <D@MMM>
2017-01-04 13:08:12 +0300

    publ1

--- Wpf Safe/MainWindow.xaml.cs
+++ Wpf Safe/MainWindow.xaml.cs
@@ -12,17 +12,108 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Collections.ObjectModel;
+using mySafe;
 
 namespace Wpf_Safe
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    
+    
     public partial class MainWindow : Window
     {
+
+        private int safeSize;
+        private List<List<CheckBox>> ChkBxGrid;
+        private Safe safe;
+      
+        
+        private ObservableCollection<string> results;
+      
         public MainWindow()
         {
             InitializeComponent();
+            results = new ObservableCollection<string> { "Результат:"};
+            listBox.ItemsSource = results;
         }
+
+        private  void DrawSafe()  //Функция вывода на экран
+        {
+
+            for (int i = 0; i < safeSize; i++)
+                for (int j = 0; j < safeSize; j++)
+                     ChkBxGrid[i][j].IsChecked = safe.safeArr[i, j]; 
+       
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e) // Если ведено целое значение - создаем новый сейф
+        {
+            try 
+            {
+                safeSize = Convert.ToInt32(textBox.Text);
+                unfGrid.Children.Clear();
+                safe = new Safe(safeSize);
+
+                label1.Content = "";
+
+
+                ChkBxGrid = new List<List<CheckBox>>();     // в этот раз воспользумеся List при хранении массива для разнообразия
+                for (int i = 0; i < safeSize; i++)          //Создаем массив чекбоксов, который будет отображать состояние сейфа
+                {
+                    ChkBxGrid.Add(new List<CheckBox>());
+                    for (int j = 0; j < safeSize; j++)
+                    {
+                        ChkBxGrid[i].Add(new CheckBox());
+                        ChkBxGrid[i][j].Content = i.ToString()+":"+j.ToString(); //запоминаем индексы, чтобы легче обрабатывать нажатие на рычаг
+                        ChkBxGrid[i][j].Style = (Style)FindResource("CheckBoxStyle1"); // используем стиль чекбоксов в виде рычагов, который создали в Blend. Пытаемся достич максимально масштабируемости
+                        ChkBxGrid[i][j].Click += Safe_Click;
+                        ChkBxGrid[i][j].Loaded += Safe_Loaded;
+                        ChkBxGrid[i][j].IsChecked = false;
+                    }
+                }
+                unfGrid.Rows = safeSize; //Разбиваем наш UniformGrid на массив NxN и выводим в каждую ячейку по рычажку
+                unfGrid.Columns = safeSize;
+                for (int i = 0; i < safeSize; i++)
+                    for (int j = 0; j < safeSize; j++)
+                        unfGrid.Children.Add(ChkBxGrid[i][j]);
+                safe.Generate2();  // тут используем второй способ генерации начального состояния
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверный формат ввода размера сейфа");
+            }
+
+        }
+
+
+
+        private void Safe_Click(object sender, RoutedEventArgs e) //Обработка нажатия на рычаг
+        {
+           
+            string ss = (string)(sender as CheckBox).Content; //Получаем индекс нажатого элемента
+            string[] s = ss.Split(new char[] { ':' });
+            safe.SwitchLever(int.Parse(s[0]), int.Parse(s[1]));
+            DrawSafe();
+            if (safe.Check())
+                {
+                    results.Add("Удачно разложено за " + safe.iIntr + "ходов"); 
+                    
+                    unfGrid.Children.Clear();
+                label1.Content = "Сейф открыт!!!";
+                    
+                }
+
+        }
+
+        private void Safe_Loaded(object sender, RoutedEventArgs e) //По окончанию создания рычагов выводим первоначальное состояние
+        {
+            string ss = (string)(sender as CheckBox).Content; 
+            string[] s = ss.Split(new char[] { ':' });
+            ChkBxGrid[int.Parse(s[0])][int.Parse(s[1])].IsChecked = safe.safeArr[int.Parse(s[0]), int.Parse(s[1])];
+
+
+        }
+
+
     }
 }
---
+++ Wpf Safe/Safe.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace mySafe
+{
+    class Safe
+    {
+        public int iIntr = 0;
+        public Boolean[,] safeArr; //здесь храним двоичное представление нашего сейа
+        private int safeSize;
+
+        public Safe( int sfSize)
+        {
+            iIntr = 0;
+            safeArr = new Boolean[sfSize, sfSize];
+            safeSize = sfSize;
+            for (int i = 0; i < sfSize; i++)
+                for (int j = 0; j < sfSize; j++)
+                    safeArr[i,j] = false; //инициализируем массив 0( все рычаги горизонтально)
+        }
+ 
+        public Boolean Check() // Проверка выполнения условия на открытие сейфа
+        {
+            Boolean allChecked = true, allUnchecked = false;
+            for (int i = 0; i < safeSize; i++)
+                for (int j = 0; j < safeSize; j++)
+                {
+                    allChecked &= safeArr[i, j];
+                    allUnchecked |= safeArr[i, j];
+                }
+            return allChecked | !allUnchecked;
+        }
+
+        public void SwitchLever(int xPos, int yPos) // поворот элемента
+        {
+            iIntr++;
+            for (int i = 0; i < safeSize; i++)
+            {
+                if (i != xPos) safeArr[i, yPos] = !safeArr[i, yPos];
+            }
+            for (int j = 0; j < safeSize; j++)
+                safeArr[xPos, j] = !safeArr[xPos, j];
+        }
+
+        public void Generate()  //Создание псевдослучайного положения рукояток
+                                  /*  Данный метод случйно нажимает рукоятки, и если сейф не пришелк открытому состоянию случайно считает генерацию
+                                   *  случайного положения выполненой, так как всегда можно провести обратные манипуляции и получить решение.
+                                   *  Такой способ менее ресурсозатратен, проще, и в игре я сделал бы именно так.
+                                   *  (здесь можно было бы закончить класс, но для сравнения реализовал второй вариант генерации ниже)
+                                   */
+        {
+            Random rand = new Random();
+            for (int i = 1; i < safeSize*2; i++)
+             { 
+                 SwitchLever(rand.Next(0, safeSize), rand.Next(0, safeSize));
+             }
+            iIntr = 0;
+        }
+
+
+        /********************************************************************************************************/
+        /*                           Создаем честную генерацию первоначального поля                             */
+        /********************************************************************************************************/
+
+        public Boolean Check(Boolean[,] tstarr) // Перегружаем функцию для любого массива safeSize
+        {
+            Boolean allChecked = true, allUnchecked = false;
+            for (int i = 0; i < safeSize; i++)
+                for (int j = 0; j < safeSize; j++)
+                {
+                    allChecked &= tstarr[i, j];
+                    allUnchecked |= tstarr[i, j];
+                }
+            return allChecked | !allUnchecked;
+        }
+
+        public void SwitchLever(int xPos, int yPos, Boolean[,] tstarr) // перегружаем поворот элемента 
+        {
+            iIntr++;
+            for (int i = 0; i < safeSize; i++)
+            {
+                if (i != xPos) tstarr[i, yPos] = !tstarr[i, yPos];
+            }
+            for (int j = 0; j < safeSize; j++)
+                tstarr[xPos, j] = !tstarr[xPos, j];
+        }
+
+        public void Generate2() // Генерация начального поля через задание случайного положения переключателей+проверку решаемости
+        {
+            Random rand = new Random();
+            Boolean solvFound = false;
+            Boolean[,] testArray = new Boolean[safeSize, safeSize];
+            Boolean[,] changermtx = new Boolean[safeSize, safeSize];
+            do
+            {
+                for (int i = 0; i < safeSize; i++) // заполняем тестовый массив случайными значениями
+                    for (int j = 0; j < safeSize; j++)
+                    {
+                        testArray[i, j] = rand.Next(0, 2) == 1 ? true : false;
+                        changermtx[i, j] = testArray[i, j]; // в алгоритме будем по очереди нажимать все вертикальные рукоятки, для этого делаем матрицу урпавления нажатиями
+                        safeArr[i, j] = testArray[i, j];
+                    }
+
+                for(int trycount=1;trycount<=3;trycount++) // пытаемся за 4 прохода поля решить задачу
+                {
+                    for (int i = 0; i < safeSize; i++)
+                    {
+                        for (int j = 0; j < safeSize; j++)
+                        {
+                            if (changermtx[i, j])
+                            {
+                                SwitchLever(i, j, testArray);
+                                if (Check(testArray))
+                                    { solvFound = true; break; } //Если найдено решение выходим из поиска
+                            }
+                        }
+                        if (solvFound) break;                      
+                    }
+                    if (solvFound) break;
+
+                    for (int i = 0; i < safeSize; i++) // Перезаписываем матрицу управления рычагами для повтороения операции нажатия на все рычаги
+                        for (int j = 0; j < safeSize; j++)
+                        {
+                            changermtx[i, j] = testArray[i, j]; 
+                        }
+                }
+
+                if (Check()) solvFound = false; // Проверяем, что случайно расположенные рукоятки не соотвтетвуют открытому сейфу
+            } while (solvFound == false); // если мы выходим из цикла - значит алгоритм нашел решение и в afeArr[i, j] сохранен правильный массив состояний
+            iIntr = 0;
+        }
+
+    }
+}
